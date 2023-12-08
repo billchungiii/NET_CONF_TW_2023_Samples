adaeeabfe410025575b7e128816b5822e2a59ffd
Bill Chung <bill_chungiii@hotmail.com>
2023-12-08 23:10:17 +0800

    rename

--- InterceptorsSample001/Program.cs
+++ InterceptorsSample001/Program.cs
@@ -7,14 +7,14 @@ namespace InterceptorsSample001
     {
         static void Main(string[] args)
         {
-            var example = new Example();
+            var example = new MyClass();
             example.MyMethod("World");
             example.MyMethod("World");
             example.MyMethod("World");
         }
     }
 
-    public class Example
+    public class MyClass
     {
         public void MyMethod(string param)
         {
@@ -25,27 +25,27 @@ namespace InterceptorsSample001
     public static class GeneratedCode
     {
         [InterceptsLocation("""C:\NETConf\2023\NET_CONF_TW_2023_Samples\InterceptorsSample001\Program.cs""", line: 11, character: 21)]
-        [InterceptsLocation("""C:\NETConf\2023\NET_CONF_TW_2023_Samples\InterceptorsSample001\Program.cs""", line: 12, character: 21)]
-        public static void MyInterceptorMethod(this Example example, string param)
+        [InterceptsLocation("""C:\NETConf\2023\NET_CONF_TW_2023_Samples\InterceptorsSample001\Program.cs""", 12, 21)]
+        public static void MyInterceptorMethod(this MyClass instance, string param)
         {
             Console.WriteLine($"Interceptor {param}");
         }
 
-        [InterceptsLocation("""C:\NETConf\2023\NET_CONF_TW_2023_Samples\InterceptorsSample001\Program.cs""", line: 13, character: 21)] 
-        public static void LoggingInterceptorMethod(this Example example, string param)
+        [InterceptsLocation("""C:\NETConf\2023\NET_CONF_TW_2023_Samples\InterceptorsSample001\Program.cs""", 13, 21)]
+        public static void LoggingInterceptorMethod(this MyClass instance, string param)
         {
             Console.WriteLine("Before...");
-            example.MyMethod(param);
+            instance.MyMethod(param);
             Console.WriteLine("After...");
         }
-    } 
+    }
 }
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
     public sealed class InterceptsLocationAttribute : Attribute
     {
-        public InterceptsLocationAttribute(string filePath, int line, int character)
+        public InterceptsLocationAttribute(string path, int line, int character)
         {
         }
     }
